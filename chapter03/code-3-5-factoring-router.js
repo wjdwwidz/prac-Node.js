0b3c2cf0f35c5738abaea5668535f384ba519847
wjdwwidz <wjdwwidz@gmail.com>
2024-02-28 16:55:47 +0900

    Feat : map을 이용한 라우터 리팩토링

---
+++ chapter03/code-3-5-factoring-router.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const url = require("url");
+
+http
+  .createServer((req, res) => {
+    const path = url.parse(req.url, true).pathname;
+    res.setHeader("Content-Type", "text/html");
+    if (path in urlMap) {
+      urlMap[path](req, res);
+    } else {
+      notFound(req, res);
+    }
+  })
+  .listen("3000", () => console.log("라우터를 리팩터링해보자!"));
+
+const user = (req, res) => {
+  const userInfo = url.parse(req.url, true).query;
+  res.end(`[user] name : ${userInfo.name} , age: ${userInfo.age}`);
+};
+
+const feed = (req, res) => {
+  res.end(`<ul>
+      <li>picture1</li>
+      <li>picture2</li>
+      <li>picture3</li>
+      </ul>
+      `);
+};
+
+const notFound = (req, res) => {
+  res.statusCode = 404;
+  res.end("404 page not found");
+};
+
+const urlMap = {
+  "/": (req, res) => res.end("HOME"),
+  "/user": user,
+  "/feed": feed,
+};
+
+// const abc = { a: 1, b: 2, c: 3 };
+// "a" in abc; //true
+// "b" in abc; //true
+// "c" in abc; //true
+// "d" in abc; //false
