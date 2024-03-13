1b1e37075e807badabd7a98aa7109e338308d1fc
wjdwwidz <wjdwwidz@gmail.com>
2024-03-13 16:41:50 +0900

    Comment : Promise 예제 추가 후 주석처리

--- chapter05/callback-promise-async-await/async-await.js
+++ chapter05/callback-promise-async-await/async-await.js
@@ -2,13 +2,19 @@ async function myName() {
   return "Andy";
 }
 
+//showName()도 async가 붙어있으니 Promise이다
 async function showName() {
   const name = await myName();
   //await 은 Promise 객체인 myName()함수의 실행이 끝나길 기다린다.
-  //출력 결과에서 pending은 console.log(showName())의 결괏값이다.
-  //showName()도 async가 붙어있으니 Promise이다
 
   console.log(name); //=> Andy출력
 }
+
 console.log(showName()); //=> Promise { <pending> } 출력
+
+// async function consoleAfterAll() {
+//   console.log(await showName());
+// }
+// consoleAfterAll(); //=> undefined 출력
+
 // showName(); => Andy 출력
