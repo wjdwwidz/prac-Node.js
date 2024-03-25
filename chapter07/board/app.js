b61d649793d0c6df4eb8cd9a96e38cedd938c0f1
wjdwwidz <wjdwwidz@gmail.com>
2024-03-25 19:49:38 +0900

    Feat : Add initial Write UI

--- chapter07/board/app.js
+++ chapter07/board/app.js
@@ -9,7 +9,11 @@ app.set("views", __dirname + "/views"); //뷰 디렉터리를 views로 설정
 
 // 라우팅 설정
 app.get("/", (req,res)=>{
-	res.render("home",{title:"안녕하세요", message:"만나서 반갑습니다!"});
+	res.render("home",{title:"테스트 게시판"});
+});
+
+app.get("/write",(req,res)=>{
+	res.render("write",{title:"테스트 게시판"});
 });
 
 app.listen(3000);
