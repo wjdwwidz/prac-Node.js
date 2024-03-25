9f78fdff3264b7310401ff5ef174f516344f527c
wjdwwidz <wjdwwidz@gmail.com>
2024-03-25 21:14:31 +0900

    Feat : Add Detail endpoint

--- chapter07/board/app.js
+++ chapter07/board/app.js
@@ -11,9 +11,13 @@ app.set("views", __dirname + "/views"); //뷰 디렉터리를 views로 설정
 app.get("/", (req,res)=>{
 	res.render("home",{title:"테스트 게시판"});
 });
-
 app.get("/write",(req,res)=>{
 	res.render("write",{title:"테스트 게시판"});
+});
+app.get("/detail/:id",async(req,res)=>{
+	res.render("detail",{
+		title:"테스트 게시판",
+	});
 });
 
 app.listen(3000);
