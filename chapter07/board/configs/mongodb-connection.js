10b998aac911f2d68230a2bdb06688dc60942785
wjdwwidz <wjdwwidz@gmail.com>
2024-03-27 13:35:59 +0900

    modify uri

--- chapter07/board/configs/mongodb-connection.js
+++ chapter07/board/configs/mongodb-connection.js
@@ -1,6 +1,5 @@
 const {MongoClient}	= require('mongodb');
-const url = 'mongodb://localhost:27017/board';
-
+const url = "mongodb+srv://wjdwwidz:nodewjdwwidz@cluster0.5fd3khr.mongodb.net/board"
 module.exports = function(callback){
 	return MongoClient.connect(url, callback);
 }
