4ed3ca664551256e09bb43b5b587125eaaea471d
wjdwwidz <wjdwwidz@gmail.com>
2024-04-15 18:58:31 +0900

    Remove

--- bun_chapter08/blog/src/hello.main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { HelloModule } from "./hello.module";
-
-async function bootstrap(){
-    const app = await NestFactory.create(HelloModule);
-    await app.listen(3000, ()=>{console.log("Hello NestJS : Server run by Bun!")}); 
-}
-
-bootstrap();
