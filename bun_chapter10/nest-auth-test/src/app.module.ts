d2e0515de8db6aa7749aec2b290931bb78ea6b85
wjdwwidz <wjdwwidz@gmail.com>
2024-05-08 13:21:27 +0900

    Feat : Init User Service, Controller, Module

--- bun_chapter10/nest-auth-test/src/app.module.ts
+++ bun_chapter10/nest-auth-test/src/app.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
 
-@Module({})
+@Module({
+  controllers: [UserController],
+  providers: [UserService],
+  imports: [UserModule]
+})
 export class AppModule {}
