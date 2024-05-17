1caab303a508070172308955b3bf65762dbceab4
wjdwwidz <wjdwwidz@gmail.com>
2024-05-17 12:26:48 +0900

    Refactor

--- bun_chapter13/echo-websocket/server.js
+++ bun_chapter13/echo-websocket/server.js
@@ -5,7 +5,7 @@ server.on('connection', ws => {
     ws.send('[서버 접속 완료!]');
 
     ws.on('message',message => {
-       we.send(`서버로부터 응답 : ${message}`);
+        ws.send(`서버로부터 응답 : ${message}`);
     });
 
     ws.on('close',()=>{
