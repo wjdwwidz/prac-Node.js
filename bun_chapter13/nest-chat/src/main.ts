bf9ae8420b01df0ac3a2c06efb0989092c0bae23
wjdwwidz <wjdwwidz@gmail.com>
2024-09-12 23:57:18 +0900

    Feat : Add route

--- bun_chapter13/nest-chat/src/main.ts
+++ bun_chapter13/nest-chat/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
